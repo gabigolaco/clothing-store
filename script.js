c0209ad97b6be9a6f5e238a67262512f9194b500
Gabi <63184250+gabigolaco@users.noreply.github.com>
2024-12-12 18:37:42 -0300

    i changed the design of the boxes and created a new category

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function debugImage() {
-    let logo = document.getElementById('logo');
-
+    let logo = document.getElementById('logo'); 
     
     switch (true) {
         case window.innerWidth >= 300 && window.innerWidth < 700:
@@ -18,10 +17,10 @@ function debugImage() {
 }
 
  let itemData = [
-        {price: '$2.00', stars: '12345'},
-        {price: '$3.00', stars: '12345'},
-        {price: '$4.00', stars: '12345'},
-        {price: '$5.00', stars: '12345'}
+        {price: 'R$2.00', stars: '12345'},
+        {price: 'R$3.00', stars: '12345'},
+        {price: 'R$4.00', stars: '12345'},
+        {price: 'R$5.00', stars: '12345'}
     ]
 
 
@@ -31,7 +30,7 @@ function loadItensTablet() {
     productContainers.forEach((item, index) => {  
         let box = document.createElement('div')
         box.classList.add('item')
-        box.setAttribute('data-price', itemData[index].price || '$1.00')    
+        box.setAttribute('data-price', itemData[index].price || 'R$1.00')    
         box.setAttribute('data-stars', itemData[index].stars || '0')    
         item.appendChild(box)
             });        
@@ -41,11 +40,10 @@ function loadItensDesktop() {
     let productContainers = document.querySelectorAll('.products-container')
 
     productContainers.forEach((item, index) => {  
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 1; i++) {
             let box = document.createElement('div')
             box.classList.add('item')
-            box.setAttribute('data-price', itemData[index].price || '$1.00')    
-            box.setAttribute('data-stars', itemData[index].stars || '0')    
+            box.setAttribute('data-price', itemData[index].price || 'R$1.00')    
             item.appendChild(box)
         }
             });        
@@ -57,34 +55,33 @@ let width = window.innerWidth;
         
     productContainers.forEach((item, index) => {
     if (width < 700 && item.children.length > 3) {
-    item.children[item.children.length - 1].remove();
-        }
+        item.children[item.children.length - 1].remove();
+    }
     
     if (width < 1039 && item.children.length > 4) {
     item.children[item.children.length - 1].remove();
     }
     
     if (width > 1039 && item.children.length < 4) {
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 1; i++) {
     let box = document.createElement('div');
     box.classList.add('item');
-    box.setAttribute('data-price', itemData[index].price || '$1.00');
-    box.setAttribute('data-stars', itemData[index].stars || '0');
+    box.setAttribute('data-price', itemData[index].price || 'R$1.00');
     item.appendChild(box);
 }
     }
 
-    if (width > 1039 && item.children.length === 4) {
+    if (width > 1039 && item.children.length === 3) {
     let box = document.createElement('div');
     box.classList.add('item');
-    box.setAttribute('data-price', itemData[index].price || '$1.00');
-    box.setAttribute('data-stars', itemData[index].stars || '0');
+    box.setAttribute('data-price', itemData[index].price || 'R$1.00');
     item.appendChild(box);
     }
     });
-    }
+        }
     
 
-window.addEventListener('resize', checkBox)
-window.addEventListener('load', debugImage)
+window.addEventListener('resize', checkBox);
+window.addEventListener('load', debugImage);
+window.addEventListener('load', loadStars)
 
